264d521e96f95d59c2f2c17153960762df2b641f
ZhangTengda <zhangtengda0315@163.com>
2019-03-19 19:12:26 +0800

    富文本编辑器2

---
+++ app/src/main/java/com/testone/demo/activity/richtext/RichText2Activity.java
@@ -0,0 +1,17 @@
+package com.testone.demo.activity.richtext;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.testone.demo.R;
+import com.testone.demo.activity.BaseActivity;
+
+public class RichText2Activity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rich_text2);
+    }
+}
